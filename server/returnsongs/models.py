eeef9205ab61e5c77b0dca87b866b3bfb8fa2deb
madhusudancs@gmail.com <madhusudancs@gmail.com@dc1e5740-0c0e-11de-9036-69e38a880166>
2009-05-02 17:46:01 +0000

    Corrected Model definition errors of not defining max_length for CharField and changing the order of Model definition.
    git-svn-id: https://astute-song-hunter.googlecode.com/svn/trunk@39 dc1e5740-0c0e-11de-9036-69e38a880166

--- server/returnsongs/models.py
+++ server/returnsongs/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-# Create your models here.
-
-class SongTag(models.Model):
-    name = models.CharField()
-    path = models.FilePathField()
-    slope = models.FloatField()
-    max_list = models.ForeignKey(TagsMaxList)
-    min_list = models.ForeignKey(TagsMinList)    
-    
+# Create your models here.  
 class TagsMaxList(models.Model):
     max_list = models.FloatField()
     
 class TagsMinList(models.Model):
     min_list = models.FloatField()
+
+class SongTag(models.Model):
+    name = models.CharField(max_length=128)
+    path = models.FilePathField(max_length=1024)
+    slope = models.FloatField()
+    max_list = models.ForeignKey(TagsMaxList)
+    min_list = models.ForeignKey(TagsMinList)    
 
