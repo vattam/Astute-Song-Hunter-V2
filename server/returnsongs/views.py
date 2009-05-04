78921c845f23e04e8ca01a7ee3d354d5d02591aa
madhusudancs@gmail.com <madhusudancs@gmail.com@dc1e5740-0c0e-11de-9036-69e38a880166>
2009-05-04 14:34:43 +0000

    Added views for bulk adding of song tags.
    git-svn-id: https://astute-song-hunter.googlecode.com/svn/trunk@51 dc1e5740-0c0e-11de-9036-69e38a880166

--- server/returnsongs/views.py
+++ server/returnsongs/views.py
@@ -1,4 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
+from server.returnsongs.models import SongTag, TagsMinList, TagsMaxList
+from server.returnsongs.all import main
+
 # Create your views here.
+def bulk_add_tags(request):
+    return render_to_response('returnsongs/bulk-add.html')
+
+def tags_added(request):
+    tag_dir = request.POST['tagdir']
+    
+    if tag_dir[-1] != '/':
+        tag_dir = tag_dir + '/'
+    
+    tag_data = main(tag_dir)
+    return HttpResponse(tagdata)
+
+def retrievesongs(request):
+    pass
 
 ## task to be done once a request is obtained
 
@@ -16,3 +36,4 @@
     #         ) is True;
 
 #4. return Intersection of 2 lists
+
