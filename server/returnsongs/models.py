8aa3a5f7e09f5f5859fa3816eae7d192ae535c17
madhusudancs@gmail.com <madhusudancs@gmail.com@dc1e5740-0c0e-11de-9036-69e38a880166>
2009-05-04 14:38:04 +0000

    Changed model definitions to follow conventional ER Schema mapping.
    git-svn-id: https://astute-song-hunter.googlecode.com/svn/trunk@55 dc1e5740-0c0e-11de-9036-69e38a880166

--- server/returnsongs/models.py
+++ server/returnsongs/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # Create your models here.  
-class TagsMaxList(models.Model):
-    max_list = models.FloatField()
-    
-class TagsMinList(models.Model):
-    min_list = models.FloatField()
-
 class SongTag(models.Model):
     name = models.CharField(max_length=128)
     path = models.FilePathField(max_length=1024)
     slope = models.FloatField()
-    max_list = models.ForeignKey(TagsMaxList)
-    min_list = models.ForeignKey(TagsMinList)    
+
+class TagsMaxList(models.Model):
+    song_id = models.ForeignKey(SongTag)
+    max_list = models.FloatField()
+    
+class TagsMinList(models.Model):
+    song_id = models.ForeignKey(SongTag)
+    min_list = models.FloatField()
 
