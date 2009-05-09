d191854bc64338f60aabd88cc8b42afa4a6d0a18
puneeth.bhat <puneeth.bhat@dc1e5740-0c0e-11de-9036-69e38a880166>
2009-05-09 04:57:55 +0000

    Have aded some testing statements just for trial purpose.
    git-svn-id: https://astute-song-hunter.googlecode.com/svn/trunk@68 dc1e5740-0c0e-11de-9036-69e38a880166

--- client/search.py
+++ client/search.py
@@ -6,7 +6,7 @@ def GetSongs(T, ServerName):
     ServerName += '/'
 
   ServerName += "ash/retrievesongs/"
-  Post = urllib.urlencode({'Slope' : T.Slope, 'MaxList' : T.Max_List, 'MinList' : T.Min_List })
+  Post = urllib.urlencode({'Slope' : T.Slope, 'MaxVar' : T.Max_List, 'MinVar' : T.Min_List })
 
   try :
     Response = urllib.urlopen(ServerName,Post)
@@ -14,4 +14,7 @@ def GetSongs(T, ServerName):
     return -1
 
   Data = Response.read()
+  f = open("some.html","w")
+  f.writelines(Data)
+  f.close()
   return {"Song_1.mp3":1, "Song_2.mp3":2, "Song_3.mp3":3, "Song_4.mp3":4, "Song_5.mp3":4}
